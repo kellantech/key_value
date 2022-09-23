2884398708f9645e032b2d0a3aa215cad94e11aa
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-09-23 10:50:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from flask import Flask, request
-
+import json
 app = Flask(__name__)
 
+with open("config.json", "r") as f:
+	config = json.load(f)
+d_ip = config["disallow-ip"]
+
 d = {}
+@app.before_request
+def before_req():
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+    	ip = (request.environ['REMOTE_ADDR'])
+    else:
+    	ip = (request.environ['HTTP_X_FORWARDED_FOR']) 
+    if ip in d_ip:
+       return '<h1>401 Unauthorized</h1>', 401
+
 @app.route('/')
 def index():
-    return '!'
+	return '!'
 @app.route('/set')
 def set():
-	
 	key = request.args.get('key')
 	val = request.args.get('val')
 	if key == None or val == None:
@@ -32,6 +44,7 @@ def all():
 	for k, v in d.items():
 		r+=f"{k}:{v}<br>"
 	return r
+	
 @app.after_request
 def add_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
