6acbac895c1aa5c1a3d619cc913050b429de5e30
Kellan Butler <87137401+kellantech@users.noreply.github.com>
2022-10-01 12:27:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,78 +1,99 @@
 from flask import Flask, request
-import json,cryptocode
-def enc(string,key):
-	return cryptocode.encrypt(string,key)
-def dec(encrypt,key):
-	return cryptocode.decrypt(encrypt,key)
+import json, cryptocode
+
+
+def enc(string, key):
+    return cryptocode.encrypt(string, key)
+
+
+def dec(encrypt, key):
+    return cryptocode.decrypt(encrypt, key)
+
 
 app = Flask(__name__)
 with open("config.json", "r") as f:
-	config = json.load(f)
+    config = json.load(f)
 a_ip = config["allow-ip"]
-enc_key= config['enc-key']
+enc_key = config['enc-key']
 d = {}
-print(enc_key)
 if a_ip == [] or a_ip == "":
-	raise ValueError("IP access rules required")
+    raise ValueError("IP access rules required")
 if isinstance(a_ip, list) == True:
-	for e in a_ip:
-		if e.endswith("*"):
-			n = e[:-1]
-			op_list = []
-			for i in range(0,256):
-				op_list.append(f"{n}{i}")
-			a_ip.remove(e)	
-			a_ip.extend(op_list)
-print(a_ip)
+    for e in a_ip:
+        if e.endswith("*"):
+            n = e[:-1]
+            op_list = []
+            for i in range(0, 256):
+                op_list.append(f"{n}{i}")
+            a_ip.remove(e)
+            a_ip.extend(op_list)
+
+
 @app.before_request
 def before_req():
-		if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
-			ip = (request.environ['REMOTE_ADDR'])
-		else:
-			ip = (request.environ['HTTP_X_FORWARDED_FOR'])
-		check = 1
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        ip = (request.environ['REMOTE_ADDR'])
+    else:
+        ip = (request.environ['HTTP_X_FORWARDED_FOR'])
+    check = 1
 
-		if a_ip == "*":
-			check = 0
-		if bool(check):	
-			if not(ip in a_ip):
-				return '<h1>401 Unauthorized</h1>', 401 
-	
-			
+    if a_ip == "*":
+        check = 0
+    if bool(check):
+        if not (ip in a_ip):
+            return '<h1>401 Unauthorized</h1>', 401
+
+
 @app.route('/')
 def index():
-	return '!'
+    return '!'
+
+
 @app.route('/set')
 def set():
-	key = request.args.get('key')
-	val = request.args.get('val')
-	if key == None or val == None:
-		return '<h1>422 Unprocessable Entity</h1>', 422
-	d[key] = enc(val,enc_key)
-	return ''
-	
+    key = request.args.get('key')
+    val = request.args.get('val')
+    if key == None or val == None:
+        return '<h1>422 Unprocessable Entity</h1>', 422
+    d[key] = enc(val, enc_key)
+    return ''
+
+
 @app.route('/get')
 def get():
-	print(d)
-	key = request.args.get('key')
-	if key == None:
-		return '<h1>422 Unprocessable Entity</h1>', 422
-	try: 
-		return dec(d[key],enc_key)
-	except KeyError:
-		return '<h1>422 Unprocessable Entity</h1>'
+
+    key = request.args.get('key')
+    enk = request.args.get('enkey')
+    res = str(dec(d[key], enk))
+    if key == None:
+        return '<h1>422 Unprocessable Entity</h1>', 422
+    try:
+        return res if res != "False" else "bad encryption key"
+    except KeyError:
+        return '<h1>422 Unprocessable Entity</h1>'
+
 
 @app.route('/all')
 def all():
-	r = ''
-	for k, v in d.items():
-		r+=f"{k}:{dec(v,enc_key)}<br>"
-	return r
-	
+    enk = request.args.get('enkey')
+    r = ''
+    for k, v in d.items():
+        res = dec(v, enk) if dec(v, enk) != False else "bad encryption key"
+        r += f"{k}:{res}<br>"
+    return r
+
+
 @app.after_request
 def add_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
+
+
+@app.route("/del")
+def delkey():
+    key = request.args.get('key')
+    del d[key]
+    return ''
 
 
 app.run(host='0.0.0.0', port=5000)
